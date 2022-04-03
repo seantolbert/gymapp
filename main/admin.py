22534dd5b59083e5ec773fe63b71b0f63b5c83c8
Sean Tolbert <seantolbert@Seans-MacBook-Pro.local>
2022-04-03 14:00:47 -0400

    adds admin registration

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Tag, Exer, Workout
 
-# Register your models here.
+admin.site.register(Workout)
+admin.site.register(Category)
+admin.site.register(Exer)
+admin.site.register(Tag)
